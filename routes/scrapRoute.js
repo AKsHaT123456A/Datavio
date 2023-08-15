1c1cc1071e15e9478d9039e0f590c626f18af4c1
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-08-15 20:33:46 +0530

    Feat:Cheerio Implemented

--- routes/scrapRoute.js
+++ routes/scrapRoute.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const { scrap } = require('../controllers/scrapController');
 const { auth } = require('../middleware/auth');
 
-router.post('/scrap', auth, scrap);
+router.post('/scrape', auth, scrap);
 
 module.exports = router;
